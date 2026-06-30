1cdf24de2bfcdf2a3c52c57bdbbe2975425aefa9
Sneha Ahirrao <snehaahirrao00@gmail.com>
2026-06-30 15:46:37 +0530

    Time: 3 ms (88.9%), Space: 47.5 MB (84.49%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,30 +1,33 @@
+import java.util.Arrays;
+
 class Solution {
     public int candy(int[] ratings) {
+
         int n = ratings.length;
+        int[] ans = new int[n];
 
-        int[] candies = new int[n];
+        Arrays.fill(ans, 1);
 
-        for (int i = 0; i < n; i++) {
-            candies[i] = 1;
-        }
-
+        
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+                ans[i] = ans[i - 1] + 1;
             }
         }
 
+        
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+                ans[i] = Math.max(ans[i], ans[i + 1] + 1);
             }
         }
 
-        int total = 0;
-        for (int candy : candies) {
-            total += candy;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += ans[i];
         }
 
-        return total;
+        return sum;
     }
 }
