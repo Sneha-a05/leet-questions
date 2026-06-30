f4b1f1652acb68f8e9009f3b3dfce1388a83e49d
Sneha Ahirrao <snehaahirrao00@gmail.com>
2026-06-30 15:43:02 +0530

    Time: 3 ms (88.9%), Space: 47.4 MB (84.49%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -4,26 +4,22 @@ class Solution {
 
         int[] candies = new int[n];
 
-        // Every child gets at least one candy
         for (int i = 0; i < n; i++) {
             candies[i] = 1;
         }
 
-        // Left to right pass
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
 
-        // Right to left pass
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
 
-        // Calculate total candies
         int total = 0;
         for (int candy : candies) {
             total += candy;
